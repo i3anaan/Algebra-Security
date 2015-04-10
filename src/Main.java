f304673d62fa8937291555d86b9588ab07355cf3
i3anaan <i3anaan@live.nl>
2015-04-10 21:23:59 +0200

    Updated First assignment

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,19 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	public static final int N_START = 5;
-	public static final int N_MAX = 10;
+	public static final int N_START = 1;
+	public static final int N_MAX = 3;
 	public static String ENCRYPTED_TEXT;
-	public static final String LOOK_FOR = "security";
+	public static final String LOOK_FOR = "encryption";
 
 	
 	public static void main(String[] args) throws FileNotFoundException {
 
 		ENCRYPTED_TEXT = (new Scanner(new File("encrypted.txt"))).useDelimiter("\\A").next();
-		/*System.out.println(applyCiphers(new int[]{1,2,3},1,"aaaaaaaa"));
-		System.out.println(applyCiphers(new int[]{1,2,3},1,0,"aaaaaaaa"));
-		System.out.println(applyCiphers(new int[]{1,2,3},1,2,"aaaaaaaa"));
-		System.out.println(applyCiphers(new int[]{1,2,3},1,1,"aaaaaaaa"));
-		
-		System.out.println(applyCiphers(new int[]{0},1,0,"aaa"));
-		*/
 		for (int n = N_START; n <= Math.min(N_MAX,LOOK_FOR.length()); n++) {
 			System.out.println("Starting N: "+n);
-			tryCipherLength(n);
+			tryCipherLength(n); //Bruteforce dictionary attack
+			countLetters(n);  //Count letters, assume text only has E's
 		}
 		System.out.println("Done.");
 	}
@@ -36,14 +30,46 @@ public class Main {
 		}
 	}
 	
+	public static void countLetters(int n){
+		int[][] count = new int[n][26];
+		
+		for(int i=0;i<ENCRYPTED_TEXT.length();i++){
+			count[i%n][(int)(ENCRYPTED_TEXT.charAt(i)-'a')] = count[i%n][(int)(ENCRYPTED_TEXT.charAt(i)-'a')]+1;  
+		}
+		
+		int[] ciphers = buildCiphersMostOccuring(count);
+		
+		System.out.println(Arrays.toString(ciphers));
+		System.out.println(applyCiphers(ciphers, -1, ENCRYPTED_TEXT));
+		System.out.println();
+		
+	}
+	
+	public static int[] buildCiphersMostOccuring(int[][] count){
+		int[] max = new int[count.length];
+		int[] maxIndex = new int[count.length];
+		for(int n=0;n<count.length;n++){
+			maxIndex[n] = 0;
+			max[n] = count[n][0];
+			for(int i=1;i<count[0].length;i++){
+				if(count[n][i]>max[n]){
+					max[n] = count[n][i];
+					maxIndex[n] = i;
+				}
+			}
+		}
+		
+		for(int n=0;n<count.length;n++){
+			maxIndex[n] = (maxIndex[n] - ('e'-'a')+26) % 26;
+		}
+		
+		return maxIndex;
+	}
+	
 	public static void checkDecrypted(int[] ciphers, String attempt){
 		if(ENCRYPTED_TEXT.contains(attempt)){
 			int index = ENCRYPTED_TEXT.indexOf(attempt);
-			//System.out.println("Attempt = "+attempt);
 			System.out.println(Arrays.toString(ciphers));
-			//System.out.println("Index = "+index);
-			//System.out.println("CipherOffset = "+((ciphers.length - (index % ciphers.length)) % ciphers.length));
-			//System.out.println(ENCRYPTED_TEXT);
 			System.out.println(applyCiphers(ciphers,-1, index, ENCRYPTED_TEXT));
 			System.out.println();
 		}
